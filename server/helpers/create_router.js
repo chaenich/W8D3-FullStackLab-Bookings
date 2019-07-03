7407b4ccccfab402546f441dc8e89a5f6d7b7675
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-03 13:05:21 +0100

    All server routes implemented / tested

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,12 +5,12 @@ const createRouter = function (collection) {
 
   const router = express.Router();
 
-  //Index
+  // Index
   router.get('/', (req, res) => {
     collection
     .find()
     .toArray()
-    .then((customers) => res.json(customers))
+    .then((docs) => res.json(docs))
     .catch((err) => {
       console.error(err);
       res.status(500);
@@ -18,9 +18,69 @@ const createRouter = function (collection) {
     });
   });
 
+  // Show
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .findOne({ _id: ObjectID(id) })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+// Create
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+    .insertOne(newData)
+    .then((result) => {
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  // Destroy
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne({ _id: ObjectID(id) })
+    .then(result => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+  // Update
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+    .findOneAndUpdate(
+      { _id: ObjectID(id)},
+      { $set: updatedData },
+      {returnOriginal: false}
+    )
+    .then((result) => {
+      res.json(result.value)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  })
   return router;
-
 };
-
 
 module.exports = createRouter;
